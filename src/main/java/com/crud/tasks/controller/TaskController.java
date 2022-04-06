61412e9f7c2fa40cd0c51d888494d3ebb455ae68
MrLemonCoffee <grzkry1@wp.pl>
2022-04-06 18:04:19 +0200

    Module 15.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,31 +1,40 @@
 package com.crud.tasks.controller;
-
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
+    private final DbService service;
+    private final TaskMapper taskMapper;
     @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasksList = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasksList);
     }
-    @GetMapping(value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
-    }
-    @DeleteMapping(value = "deleteTask")
-    public void deleteTask(Long taskId) {
 
+    @GetMapping(value = "/{taskId}")
+    public TaskDto getTask(@PathVariable("taskId") Long taskId) {
+        Task task = service.getTask(taskId);
+        return taskMapper.mapToTaskDto(task);
     }
-    @PutMapping(value = "updateTask")
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+    }
+    @PutMapping
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
-    @PostMapping(value = "createTask")
+    @PostMapping
     public void createTask(TaskDto taskDto) {
-
     }
 }
